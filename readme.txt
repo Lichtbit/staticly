cd9280cb865ce6f99000fc66dda7549774c20cf0
Georg Limbach (Warnow) <georf@georf.de>
2013-08-28 23:58:10 +0200

    add first simple version with setting menu

---
+++ readme.txt
@@ -0,0 +1,10 @@
+=== staticly ===
+Tags: static, html, cache
+Requires at least: 3.0.1
+Tested up to: 3.4
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Generates static html files while normal loading.
+
---
+++ staticly.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: Staticly
+Plugin URI: http://www.mgvmedia.com/
+Description: Generates static html files while normal loading.
+Version: 1.0
+Author: georf
+Author URI: http://www.mgvmedia.com
+License: GPL3
+*/
+
+
+add_action('admin_menu',     'MGVmediaStaticly::create_menu');
+add_action('save_post',      'MGVmediaStaticly::clean');
+add_action('comment_post',   'MGVmediaStaticly::clean');
+add_action('plugins_loaded', 'MGVmediaStaticly::perform');
+
+class MGVmediaStaticly {
+    function create_menu() {
+        add_submenu_page('options-general.php', 'Staticly', 'Staticly', 7, "staticly", "MGVmediaStaticly::settingPage");
+    }
+
+    function settingPage() {
+        if (isset($_POST['remove_static_files'])) {
+            self::clean('..');
+        }
+        echo '
+            <div id="postbox-container-1" class="postbox-container">
+                <div id="side-sortables" class="meta-box-sortables ui-sortable">
+                    <div id="submitdiv" class="postbox">
+                        <h3 class="hndle"><span>Static files</span></h3>
+                        <div class="inside">
+                            <form method="post">
+                                <input type="hidden" name="remove_static_files" value="1">
+                                <button class="button-primary" type="submit">Remove static files</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>';
+
+
+        echo '<table class="widefat">';
+        echo '<thead>';
+        echo '<tr><th>filename</th><th>size</th></tr>';
+
+
+        echo '</thead><tbody>';
+        $output = explode("\n",shell_exec('find ../ -type f -name index.html'));
+        foreach ($output as $line) {
+                echo '<tr><td>',
+                    htmlspecialchars(substr($line,2)),
+                '</td><td>',
+                    filesize($line),
+                '</td></tr>';
+        }
+        echo '</tbody></table>';
+
+    }
+
+    function clean($root = false) {
+        if (!$root) $root = $_SERVER['DOCUMENT_ROOT'];
+        exec('find '.$root.'/ -type f -name index.html | xargs rm');
+        exec('find '.$root.'/ -type d -empty | xargs rmdir');
+    }
+
+    function perform() {
+        $request_uri = $_SERVER['REQUEST_URI'];
+        if (substr($request_uri, strlen($request_uri) -1) == '/'
+        && $_SERVER['SCRIPT_NAME'] == '/index.php'
+        && $_SERVER['REMOTE_ADDR'] != '127.0.0.1'
+        && $_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']) {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $_SERVER['SCRIPT_URI']);
+            curl_setopt($ch, CURLOPT_FAILONERROR, false);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+            $content = curl_exec($ch);
+
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            if ($httpCode != 200) return;
+
+
+            $content = file_get_contents($_SERVER['SCRIPT_URI']);
+            $root = $_SERVER['DOCUMENT_ROOT'];
+            if (!is_dir($root.$request_uri)) mkdir($root.$request_uri, 0755, true);
+            file_put_contents($root.$request_uri.'index.html', $content);
+            echo $content;
+            exit();
+        }
+    }
+}
+
+
